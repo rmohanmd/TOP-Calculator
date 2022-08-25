109f3639c98afa1ae9191240e022b05bf4498861
rmohanmd <rmohan.md@gmail.com>
2022-08-25 16:17:03 -0400

    For loop to create all the necessary buttons with labels & class names

--- script.js
+++ script.js
@@ -24,3 +24,30 @@ function operate(symbol) {
     ? multiply(num1, num2)
     : divide(num1, num2);
 }
+
+const wrapper = document.querySelector(".wrapper");
+const buttonLabels = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  0,
+  "add",
+  "subtract",
+  "mulitply",
+  "divide",
+  "equal",
+  "clear",
+];
+
+for (i = 0; i < 16; i++) {
+  const button = document.createElement("button");
+  button.classList.add(`${buttonLabels[i]}`);
+  button.textContent = `${buttonLabels[i]}`;
+  wrapper.appendChild(button);
+}
